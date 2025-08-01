d4611e709b98852d7227af2e6dd21e4a9515c6b4
shubham14321 <subhamkumar44448@gmail.com>
2025-08-01 17:45:30 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// BrowserRouter ko import karne wali line ko badlein
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> ko <HashRouter> se badal dein */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </HashRouter>
+  </React.StrictMode>,
 )
